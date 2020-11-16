e9eed497cb233484a66911e7d3d30428a074a9c6
Amy Simpson <simpson.r.amy@gmail.com>
2020-11-16 23:26:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,3 +31,37 @@
 // function preserveDecimal(n) {
 //   return parseFloat(n);
 // }
+
+
+function add(a,b){
+	return (a + b);
+}
+
+function subtract(a, b){
+	return (a - b);
+}
+
+function multiple(a, b){
+	return (a * b);
+}
+
+function divide(a,b){
+	return (a / b);
+}
+
+
+function increment(n){
+	return n ++;
+}
+
+function decrement(n){
+	return n--;
+}
+
+function makeInt(n){
+	return parseInt(n, 10);
+}
+
+function perserveDecimal(n){
+	return parseFloat(n);
+}
